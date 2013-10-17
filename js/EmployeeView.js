159ec068a850e597638dd9579e2c0e8b101df745
Franz.Brandstaetter <fbrandstaetter.mmt-m2011@fh-salzburg.ac.at>
2013-10-17 15:50:51 +0200

    try contactPicker plugin

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -5,7 +5,7 @@ var EmployeeView = function(employee) {
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn', this.addToContacts);
         this.el.on('click', '.change-pic-btn', this.changePicture);
-		app.showConfirm("Notification","Test");
+	//	app.showConfirm("Notification","Test");
     };
 
     this.render = function() {
